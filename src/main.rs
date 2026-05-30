87060aad3ffbcb10b4349939d707d86eb9ab932e
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-05-31 00:25:46 +0530

    concurrency and channel(pipeline) exec

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,45 @@ use scanner::scan_port;
 use clap::Parser;
 use std::net::{TcpStream,ToSocketAddrs};
 use std::time::Duration;
+use std::thread;
+use std::sync::mpsc;
 
 fn main() {
-    let args=CliArgs::parse();
+    let mut args=CliArgs::parse();
+
+
+    let (tx,rx)=mpsc::channel();
+
+    let mut handles=vec![];
+
+    println!("scanning target {}",args.target);
+
 
     for thread_ind in 0..args.threads{
+        let thread_tx=tx.clone();
+        let target=args.target.clone();
         let start=args.start_port+ thread_ind as u16;
 
-        for port in (start..args.end_port+1).step_by(args.threads as usize){
-            println!("thread_ind : {}, port :{}",thread_ind,port);
-
+        let handle=thread::spawn(move || {
+            for port in (start..args.end_port+1).step_by(args.threads as usize){
+            if scanner::scan_port(&target, port){
+                thread_tx.send(port);
+            }
         } 
-        println!();
+        });
+        handles.push(handle);
+    }
+    drop(tx);
 
+    for open_port in rx{
+        println!("found open port {}",open_port);
     }
 
+    for handle in handles{
+        let th=handle.join();
+    }
+
+    println!("scan completed");
     // println!("port configuration");
     // println!("target : {}",args.target);
     // println!("start port {} -> end port {}",args.start_port,args.end_port);
