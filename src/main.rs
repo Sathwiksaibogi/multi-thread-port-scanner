b17b05c84670a627a5f1db8cb3832220f39b1600
Sathwiksaibogi <sathwiksaibogi05@gmail.com>
2026-05-30 23:32:26 +0530

    work distribution plan

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,24 @@ use std::net::{TcpStream,ToSocketAddrs};
 use std::time::Duration;
 
 fn main() {
-    // let args=CliArgs::parse();
+    let args=CliArgs::parse();
+
+    for thread_ind in 0..args.threads{
+        let start=args.start_port+ thread_ind as u16;
+
+        for port in (start..args.end_port+1).step_by(args.threads as usize){
+            println!("thread_ind : {}, port :{}",thread_ind,port);
+
+        } 
+        println!();
+
+    }
 
     // println!("port configuration");
     // println!("target : {}",args.target);
     // println!("start port {} -> end port {}",args.start_port,args.end_port);
     // println!("threads : {} ",args.threads);
 
-    let status_80=scanner::scan_port("google.com", 80);
-    println!("result :{}",status_80);
+//     let status_80=scanner::scan_port("google.com", 80);
+//     println!("result :{}",status_80);
 }
